877b7d6e19c9668d4177425f63ae3c2b12ea250d
Cirilla <LYFly228@gmail.com>
2019-06-03 22:30:42 +0800

    启动脚本修改

--- start.sh
+++ start.sh
@@ -12,7 +12,7 @@ git pull  # should use git clone https://name:pwd@xxx.git
 docker run --rm \
     -v $m2_cache:/root/.m2 \
     -v $proj_home:/usr/src/mymaven \
-    -w /usr/src/mymaven $img_mvn mvn clean package -U -Pdev
+    -w /usr/src/mymaven $img_mvn mvn clean package
 
 sudo mv $proj_home/target/eureka-service-*.jar $proj_home/target/demo.jar # 兼容所有sh脚本
 docker build -t $img_output .
