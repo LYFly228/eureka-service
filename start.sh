dc756ac95a0c695299ab07af48816a99460d3151
Cirilla <LYFly228@gmail.com>
2019-06-03 22:09:35 +0800

    增加启动脚本

---
+++ start.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+cd `dirname $0`
+
+img_mvn="maven:3.3.3-jdk-8"                 # docker image of maven
+m2_cache=~/.m2                              # the local maven cache dir
+proj_home=$PWD                              # the project root dir
+img_output="cirilla/eureka-service"         # output image tag
+
+git pull  # should use git clone https://name:pwd@xxx.git
+
+
+docker run --rm \
+    -v $m2_cache:/root/.m2 \
+    -v $proj_home:/usr/src/mymaven \
+    -w /usr/src/mymaven $img_mvn mvn clean package -U -Pdev
+
+sudo mv $proj_home/target/eureka-service-*.jar $proj_home/target/demo.jar # 兼容所有sh脚本
+docker build -t $img_output .
+
+mkdir -p $PWD/logs
+chmod 777 $PWD/logs
+
+# 启动镜像
+docker run -d --restart=on-failure:5 --privileged=true \
+    --net=host \
+    -w /home \
+    -v $PWD/logs:/home/logs \
+    --name eureka-service2 cirilla/eureka-service \
+    java \
+        -Djava.security.egd=file:/dev/./urandom \
+        -Duser.timezone=Asia/Shanghai \
+        -XX:+PrintGCDateStamps \
+        -XX:+PrintGCTimeStamps \
+        -XX:+PrintGCDetails \
+        -XX:+HeapDumpOnOutOfMemoryError \
+        -Xloggc:logs/gc_$version.log \
+        -jar /home/demo.jar
+
