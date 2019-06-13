7de25117eaac5b1eed26a065b8a8923c6af8f948
Cirilla <LYFly228@gmail.com>
2019-06-13 14:51:42 +0800

    脚本修改

--- start.sh
+++ start.sh
@@ -12,7 +12,7 @@ git pull  # should use git clone https://name:pwd@xxx.git
 docker run --rm \
     -v $m2_cache:/root/.m2 \
     -v $proj_home:/usr/src/mymaven \
-    -w /usr/src/mymaven $img_mvn mvn clean package
+    -w /usr/src/mymaven $img_mvn mvn clean package -Dmaven.test.skip
 
 sudo mv $proj_home/target/eureka-service-*.jar $proj_home/target/demo.jar # 兼容所有sh脚本
 docker build -t $img_output .
@@ -20,12 +20,15 @@ docker build -t $img_output .
 mkdir -p $PWD/logs
 chmod 777 $PWD/logs
 
+# 删除容器
+docker rm -f eureka-service &> /dev/null
+
 # 启动镜像
 docker run -d --restart=on-failure:5 --privileged=true \
     --net=host \
     -w /home \
     -v $PWD/logs:/home/logs \
-    --name eureka-service2 cirilla/eureka-service \
+    --name eureka-service cirilla/eureka-service \
     java \
         -Djava.security.egd=file:/dev/./urandom \
         -Duser.timezone=Asia/Shanghai \
